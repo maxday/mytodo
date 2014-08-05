4fe4513327ed4dfb9376ebe689ba1dccc7ee5187
maxday <admin@dnab404684.stanford.edu>
2014-08-05 16:36:30 -0700

    empty project

---
+++ mytodo/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  mytodo
+//
+//  Created by maxday on 2014-08-05.
+//  Copyright (c) 2014 MaximeDavid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ mytodo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  mytodo
+//
+//  Created by maxday on 2014-08-05.
+//  Copyright (c) 2014 MaximeDavid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
